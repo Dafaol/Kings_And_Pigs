f387e198d8bad1cbf599e17a5e7be61875cb1842
Dafaol <danielfagundea@gmail.com>
2023-12-04 12:45:38 -0300

    583.80 - Criando a variavel de controle do canvas vida

--- Kings and Pigs/Assets/Scripts/GameManager.cs
+++ Kings and Pigs/Assets/Scripts/GameManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     //static é uma variavel unica de classe
     private static int vida = 3;
     [SerializeField] private int vidaInicial = 3;
+
+    //variavel dos coraçoes
+    [SerializeField] private Image[] coracoes;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +22,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AjustaVida();
     }
     //criando o metodo para ir para outra cena
     public void MudaCena(string destino)
@@ -40,5 +44,26 @@ public class GameManager : MonoBehaviour
         vida = vidaInicial;
         //indo para a cena inicial
         SceneManager.LoadScene("Cena 1");
+    }
+
+    public void AjustaVida()
+    {
+        //rodar pelo meu vetor
+        for (var i=0; i < coracoes.Length; i++)
+        {
+            //checando se o valor atual é maior do que a vida atual
+            if (i < vida)
+            {
+                //eu tenho mais vidas do que o valor do i
+                //Se i for 0 , quer dizer que a vida é ao menos 1
+                //se i for 1 , quer dizer que a vida é ao menos 2
+                //se i for 2 , quer dizer que a vida é ao menos 3
+                coracoes[i].enabled = true;
+            }
+            else
+            {
+                coracoes[i].enabled = false;
+            }
+        }
     }
 }
